628182538811e12fb9ed1813f8ad4bab4fa4bbbe
EnxarxadESS <hola@lenxarxada.coop>
2025-04-20 16:34:07 +0200

    Canviat el port a 10000 per Render

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;  // Utilitzem el port 10000 per Render
 
 app.use(cors());
 app.use(express.static('public'));
@@ -9,4 +9,5 @@ app.use(express.static('public'));
 app.listen(port, () => {
   console.log(`Servidor en marxa a http://localhost:${port}`);
 });
+
 
